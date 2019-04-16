ddbe455fca89a063eeeec907926f292cbd619c74
Jaime Calvo <info@digitalconcept.se>
2019-04-16 10:29:30 +0200

    added index.html to webpack, removed old pagination file, lookup for af kommunkod

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-  module.exports = 
-  {
-    entry: './src/index.js',
-    output: {
-      filename: 'afPbWidget.js',
-      path: path.resolve(__dirname, 'public/script')
-    },
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          include: path.resolve(__dirname, 'src'),
-        }
-      ]
-    }
-  };
+module.exports = 
+{
+  entry: './src/index.js',
+  output: {
+    filename: './script/afPbWidget.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true,
+        title: 'Jobtech widget',
+        MyPageHeader: 'Jobtech widget',
+        template: './src/index.html',
+        filename: 'index.html' //relative to root of the application
+    })
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+      }
+    ]
+  }
+};
